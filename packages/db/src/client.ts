f8191d1218de5cef57f4ff73929b27903d8381c5
Daniel Dao <danieldkdao@gmail.com>
2026-08-09 11:40:11 -0500

    feat: move over API to Next.js app, setup shared DB package

---
+++ packages/db/src/client.ts
@@ -0,0 +1,19 @@
+import { neonConfig, Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import * as schema from "./schema.js";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+
+export const createDb = (databaseUrl: string) => {
+  const pool = new Pool({
+    connectionString: databaseUrl,
+  });
+
+  const db = drizzle({
+    client: pool,
+    schema,
+  });
+
+  return { pool, db };
+};
